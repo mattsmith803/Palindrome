4fa84993848b99e999ca2e24cd3e6bec1d9c20c4
mattsmith803 <mattsmith803@hotmail.com>
2021-11-12 15:59:01 -0500

    implementing unique words and counts class with test paragraph

---
+++ src/UniqueWords.cs
@@ -0,0 +1,24 @@
+namespace HelloWorld
+{
+    public class UniqueWords
+    {
+        public static void PrintUniqueWordsAndCounts(string paragraph){
+
+            Dictionary<string, int> wordsToCounts = new Dictionary<string, int>();
+
+            foreach (var word in paragraph.Split(' ')){
+                
+                if (!wordsToCounts.ContainsKey(word)){
+                    wordsToCounts.Add(word, 0);
+                }
+                
+                wordsToCounts[word] = wordsToCounts[word] + 1;
+            }
+
+            foreach (var uniqueWord in wordsToCounts){
+                Console.WriteLine(uniqueWord.Key + ": " + uniqueWord.Value);
+            }
+
+        }
+    }
+}
