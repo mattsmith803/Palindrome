c3471c1cfad7d122a9b8700c24a10a66e22a8fc1
mattsmith803 <mattsmith803@hotmail.com>
2021-11-13 11:48:06 -0500

    adding test project

---
+++ Palindrome.Test/UniqueWordsTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Palindrome.Test;
+
+[TestClass]
+public class UniqueWordsTest
+{
+    [TestMethod]
+    public void HasUniqueWords_True()
+    {
+        Assert.AreEqual(true, true);
+    }
+
+    [TestMethod]
+    public void HasUniqueWords_False()
+    {
+        Assert.AreEqual(false, false);
+    }
+}
